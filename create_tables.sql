a04985b86415225ee89b5113cea9c9c630b05fbc
lorenmh <neutronstar@gmail.com>
2017-02-06 08:43:06 -0800

    first insert

--- create_tables.sql
+++ create_tables.sql
@@ -1,5 +1,8 @@
 CREATE TABLE movie (
   serial_no VARCHAR(64),
   title VARCHAR(128),
+  prod_cost NUMBER,
+  release_date DATE,
+  language VARCHAR(64),
   PRIMARY KEY (serial_no)
 );
---
+++ insert_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO movie VALUES (
+  '1',
+  'foo_movie',
+  342000,
+  TO_DATE('2017/02/06', 'yyyy/mm/dd'),
+  'english'
+);
