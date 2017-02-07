24c3af6a3659a043fe0bb0c56840f02c9602f1ce
lorenmh <neutronstar@gmail.com>
2017-02-06 19:53:41 -0800

    adding in some constraints

--- create_tables.sql
+++ create_tables.sql
@@ -1,9 +1,11 @@
+-- Entity Definition
 CREATE TABLE movie (
-  serial_no VARCHAR(64),
-  title VARCHAR(128),
-  prod_cost NUMBER,
+  -- 16 bit GUID
+  id RAW(16) DEFAULT SYS_GUID(),
+  title VARCHAR(128) NOT NULL,
+  prod_cost NUMBER NOT NULL,
   release_date DATE,
-  language VARCHAR(64),
+  language VARCHAR(64) NOT NULL,
   PRIMARY KEY (serial_no)
 );
 
@@ -13,8 +15,10 @@ CREATE TABLE genre (
 )
 
 CREATE TABLE tv_show (
+  -- 16 bit GUID
+  id RAW(16) DEFAULT SYS_GUID(),
   network VARCHAR(64),
-  name VARCHAR(128),
+  name VARCHAR(128) NOT NULL,
   category VARCHAR(64),
   PRIMARY KEY (network, name)
 );
@@ -27,8 +31,20 @@ CREATE TABLE location (
 );
 
 CREATE TABLE season (
-  num INTEGER,
-  start_date DATE,
+  movie_id RAW(16) NOT NULL,
+  num INTEGER NOT NULL,
+  start_date DATE NOT NULL,
   end_date DATE,
-  PRIMARY KEY (num)
+  PRIMARY KEY (movie_id),
+  FOREIGN KEY (movie_id) REFERENCES movie(id)
 );
+
+CREATE TABLE episode (
+  movie_id RAW(16),
+  season_num INTEGER,
+  num INTEGER,
+  title VARCHAR(128),
+  length_minutes INTEGER,
+  PRIMARY KEY (movie_id, season_num, num),
+  FOREIGN KEY (movie_id, season_num) REFERENCES season(movie_id, num)
+)
