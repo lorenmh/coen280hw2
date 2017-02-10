e379c115758420379173e8acc826a715cfffdaac
lorenmh <neutronstar@gmail.com>
2017-02-09 22:25:27 -0800

    dumb teacher

--- createdb.sql
+++ createdb.sql
@@ -1,34 +1,25 @@
-CREATE TABLE location (
+CREATE TABLE person (
   id INTEGER,
-  nation VARCHAR(128),
-  state VARCHAR(128),
-  town VARCHAR(128),
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE celebrity (
-  id INTEGER,
-  fname VARCHAR(64),
-  lname VARCHAR(64),
+  name VARCHAR(64),
   dob DATE,
-  birth_location_id INTEGER,
+  gender CHAR(1)
+  birthplace VARCHAR(128),
+  attribute VARCHAR(128),
   PRIMARY KEY (id),
-  FOREIGN KEY (birth_location_id) REFERENCES location(id)
 );
 
 -- Entity Definition
 CREATE TABLE movie (
   id INTEGER,
   title VARCHAR(128) NOT NULL,
-  prod_cost NUMBER NOT NULL,
   release_date DATE,
-  director_celebrity_id INTEGER,
-  producer_celebrity_id INTEGER,
-  location_id INTEGER,
+  director_person_id INTEGER,
+  producer_person_id INTEGER,
+  prod_cost NUMBER,
+  location VARCHAR(128),
   PRIMARY KEY (id),
-  FOREIGN KEY (director_celebrity_id) REFERENCES celebrity(id),
-  FOREIGN KEY (producer_celebrity_id) REFERENCES celebrity(id),
-  FOREIGN KEY (location_id) REFERENCES location(id)
+  FOREIGN KEY (director_person_id) REFERENCES person(id),
+  FOREIGN KEY (producer_person_id) REFERENCES person(id),
 );
 
 CREATE TABLE language (
@@ -68,10 +59,9 @@ CREATE TABLE tv_show (
 CREATE TABLE scene (
   movie_id INTEGER,
   num INTEGER NOT NULL,
-  location_id INTEGER,
+  location VARCHAR(128),
   PRIMARY KEY (movie_id, num),
-  FOREIGN KEY (movie_id) REFERENCES movie(id),
-  FOREIGN KEY (location_id) REFERENCES location(id)
+  FOREIGN KEY (movie_id) REFERENCES movie(id)
 );
 
 CREATE TABLE season (
@@ -124,11 +114,11 @@ CREATE TABLE tv_show_nomination (
   FOREIGN KEY (award_id) REFERENCES award(id)
 );
 
-CREATE TABLE celebrity_nomination (
-  celebrity_id INTEGER,
+CREATE TABLE person_nomination (
+  person_id INTEGER,
   award_id INTEGER,
   is_winner NUMBER(1),
-  FOREIGN KEY (celebrity_id) REFERENCES celebrity(id),
+  FOREIGN KEY (person_id) REFERENCES person(id),
   FOREIGN KEY (award_id) REFERENCES award(id)
 );
 
@@ -143,63 +133,41 @@ CREATE TABLE imdb_user (
 );
 
 CREATE TABLE imdb_user_review (
-  id INTEGER,
-  imdb_user_id INTEGER NOT NULL,
   movie_id INTEGER,
-  tv_show_id INTEGER,
-  text VARCHAR(4000),
+  imdb_user_id INTEGER NOT NULL,
+  rating INTEGER,
+  votes INTEGER,
   publish_date DATE,
-  rating NUMBER,
-  PRIMARY KEY (id),
-  FOREIGN KEY (imdb_user_id) REFERENCES imdb_user(id),
   FOREIGN KEY (movie_id) REFERENCES movie(id),
-  FOREIGN KEY (tv_show_id) REFERENCES tv_show(id)
+  FOREIGN KEY (imdb_user_id) REFERENCES imdb_user(id),
 );
 
 CREATE TABLE critic_review (
-  id INTEGER,
-  celebrity_id INTEGER NOT NULL,
+  person_id INTEGER NOT NULL,
   movie_id INTEGER,
-  tv_show_id INTEGER,
   text VARCHAR(4000),
   publish_date DATE,
   PRIMARY KEY (id),
-  FOREIGN KEY (celebrity_id) REFERENCES celebrity(id),
+  FOREIGN KEY (person_id) REFERENCES person(id),
   FOREIGN KEY (movie_id) REFERENCES movie(id),
   FOREIGN KEY (tv_show_id) REFERENCES tv_show(id)
 );
 
-CREATE TABLE movie_actor (
+CREATE TABLE movie_role (
   movie_id INTEGER,
-  celebrity_id INTEGER,
+  person_id INTEGER,
   role VARCHAR(128),
   FOREIGN KEY (movie_id) REFERENCES movie(id),
-  FOREIGN KEY (celebrity_id) REFERENCES celebrity(id)
+  FOREIGN KEY (person_id) REFERENCES person(id)
 );
 
-CREATE TABLE tv_show_actor (
+CREATE TABLE tv_show_role (
   tv_show_id INTEGER,
   season_num INTEGER,
   episode_num INTEGER,
-  celebrity_id INTEGER,
+  person_id INTEGER,
   role VARCHAR(128),
   FOREIGN KEY (tv_show_id, season_num, episode_num)
       REFERENCES episode(tv_show_id, season_num, num),
-  FOREIGN KEY (celebrity_id) REFERENCES celebrity(id)
-);
-
-CREATE TABLE critic_review_vote (
-  critic_review_id INTEGER NOT NULL,
-  imdb_user_id INTEGER NOT NULL,
-  rating NUMBER,
-  category VARCHAR(64),
-  FOREIGN KEY (critic_review_id) REFERENCES critic_review(id),
-  FOREIGN KEY (imdb_user_id) REFERENCES imdb_user(id)
-);
-
-CREATE TABLE recommended_imdb_user_review (
-  celebrity_id INTEGER NOT NULL,
-  imdb_user_review_id INTEGER NOT NULL,
-  FOREIGN KEY (celebrity_id) REFERENCES celebrity(id),
-  FOREIGN KEY (imdb_user_review_id) REFERENCES imdb_user_review(id)
+  FOREIGN KEY (person_id) REFERENCES person(id)
 );
